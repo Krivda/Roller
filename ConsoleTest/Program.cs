a128b3c244699e12ecacbdb1be3a2755b2a648ad
Alice <Alice@ALICE-PC>
2017-08-25 00:09:32 +0300

    console app added

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NLog;
+using RollerEngine.Character;
+using RollerEngine.Roller;
+using RollerEngine.Rolls;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        private static readonly NLog.Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+
+            var res = HatysPartyLoader.LoadParty(new NLogLogger(logger), new OfflineDiceRoller());
+
+            res.Nameless.WeeklyBoostTeachersEase();
+            Console.ReadKey();
+        }
+    }
+}
