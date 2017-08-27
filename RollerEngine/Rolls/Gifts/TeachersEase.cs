18352af485a266b61e544699b0f696e6f802758d
Alice <Alice@ALICE-PC>
2017-08-27 03:07:56 +0300

    [Roller]
    Fixed IRoller interface
    Fixed Nameless buff
    Strarted Spiridon
    Strarted Hatys

---
+++ RollerEngine/Rolls/Gifts/TeachersEase.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using RollerEngine.Character;
+using RollerEngine.Character.Modifiers;
+using RollerEngine.Logger;
+using RollerEngine.Roller;
+
+namespace RollerEngine.Rolls.Gifts
+{
+    public class TeachersEase : GiftRoll
+    {
+        private const string GIFT_NAME = "Teacher's Ease";
+
+        public TeachersEase(IRollLogger log, IRoller roller) : base(GIFT_NAME, log, roller, new List<string>() { Build.Atributes.Manipulation, Build.Abilities.Instruction}, new List<string>())
+        {
+        }
+
+        public override int GetBaseDC(Build actor, List<Build> targets)
+        {
+            return Math.Max(10 - targets[0].Traits[Build.Atributes.Intellect], 3);
+        }
+
+        public int Roll(Build actor, Build target, string ability, bool hasSpec, bool hasWill)
+        {
+            int result = base.Roll(actor, new List<Build>() { target }, hasSpec, hasWill);
+
+            if (result > 0)
+            {
+                target.TraitModifiers.Add(
+                    //string name, List<string> traits, DurationType duration, List<string> condtions, int value, BonusTypeKind bonusType
+                    new TraitModifier(
+                        Name,
+                        new List<string>() { ability }, 
+                        DurationType.Roll, 
+                        new List<string>(), 
+                        result,
+                        TraitModifier.BonusTypeKind.AdditionalDice
+                    ));
+
+                _log.Log(Verbosity.Important, string.Format("{0} obtained bonus {1} dice on {2} rolls from {3} gift.", target.Name, result, ability,  Name));
+            }
+            else
+            {
+                _log.Log(Verbosity.Important, string.Format("{0} didn't get bonus from {1} gift.", target.Name, Name));
+            }
+
+            return result;
+        }
+    }
+}
--- UnitTests/RollchainTest.cs
+++ UnitTests/RollchainTest.cs
@@ -1,25 +1,25 @@
+using System;
+using System.IO;
 using NLog;
 using NUnit.Framework;
 using RollerEngine.Character;
 using RollerEngine.Logger;
 using RollerEngine.Roller;
-using RollerEngine.Rolls;
-using Logger = NUnit.Framework.Internal.Logger;
 
 namespace UnitTests
 {
     class RollchainTest
     {
-        private static readonly NLog.Logger logger = LogManager.GetCurrentClassLogger();
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
         [Test]
         public void TestNameLessBuff()
         {
+            var rollLogger = new NLogLogger(Logger);
+            Logger.Info("Started");
+            var res = HatysPartyLoader.LoadParty(rollLogger, new OfflineDiceRoller(rollLogger));
 
-
-            var res = HatysPartyLoader.LoadParty(new NLogLogger(logger), new OfflineDiceRoller());
-
-            res.Nameless.WeeklyBoostTeachersEase();
+            res.WeeklyLearning();
         }
     }
 }
